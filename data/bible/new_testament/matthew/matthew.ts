ba438e103668085468d31b1239665e73812cd649
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2025-04-25 05:24:17 +0000

    updated matthew 1 and matthew

--- data/bible/new_testament/matthew/matthew.ts
+++ data/bible/new_testament/matthew/matthew.ts
@@ -1,5 +1,5 @@
 import matthewChapter1 from "./matthew_chapter1";
 
 export const matthew = {
-    "1-1": matthewChapter1,
+    "40-1": matthewChapter1,
 }
--- data/bible/new_testament/matthew/matthew_chapter1.ts
+++ data/bible/new_testament/matthew/matthew_chapter1.ts
@@ -1,173 +1,173 @@
 const matthewChapter1 = {
-  "1-1": [
+  "40-1": [
     {
-      id: "1-1-1",
+      id: "40-1-1",
       verse: 1,
       text: "Ni sem somoyoŋ Yesuna, hu njafna vi Davidna, hu njafna vi Abrahamma.",
       french: "Généalogie de Jésus Christ, fils de David, fils d'Abraham.",
     },
     {
-      id: "1-1-2",
+      id: "40-1-2",
       verse: 2,
       text: "Abraham vuɗ Isaak. Isaak vuɗ Yakob. Yakob vuɗ Yuda u gorsemma.",
       french:
         "Abraham engendra Isaac; Isaac engendra Jacob; Jacob engendra Juda et ses frères;",
     },
     {
-      id: "1-1-3",
+      id: "40-1-3",
       verse: 3,
       text: "Yuda vuɗ Fares u Zara (susi ni Tamar). Fares vuɗ Esrom. Esrom vuɗ Aram.",
       french:
         "Juda engendra de Thamar Pharès et Zara; Pharès engendra Esrom; Esrom engendra Aram;",
     },
     {
-      id: "1-1-4",
+      id: "40-1-4",
       verse: 4,
       text: "Aram vuɗ Aminadab. Aminadab vuɗ Naasson. Naasson vuɗ Salmon.",
       french:
         "Aram engendra Aminadab; Aminadab engendra Naasson; Naasson engendra Salmon;",
     },
     {
-      id: "1-1-5",
+      id: "40-1-5",
       verse: 5,
       text: "Salmon vuɗ Booz, summi Rahab. Booz vuɗ Obed, summi Rut. Obed vuɗ Jese.",
       french: "Salmon engendra Boaz de Rahab; Boaz engendra Obed de Ruth;",
     },
     {
-      id: "1-1-6",
+      id: "40-1-6",
       verse: 6,
       text: "Jese vuɗ mulna David. David vuɗ Salomon u caŋ Urira.",
       french:
         "Obed engendra Isaï; Isaï engendra David. Le roi David engendra Salomon de la femme d'Urie;",
     },
     {
-      id: "1-1-7",
+      id: "40-1-7",
       verse: 7,
       text: "Salomon vuɗ Roboam. Roboam vuɗ Abiya. Abiya vuɗ Aza.",
       french:
         "Salomon engendra Roboam; Roboam engendra Abia; Abia engendra Asa;",
     },
     {
-      id: "1-1-8",
+      id: "40-1-8",
       verse: 8,
       text: "Aza vuɗ Josafat. Josafat vuɗ Joram. Joram vuɗ Oziyas.",
       french:
         "Asa engendra Josaphat; Josaphat engendra Joram; Joram engendra Ozias;",
     },
     {
-      id: "1-1-9",
+      id: "40-1-9",
       verse: 9,
-      text: "Oziyas vuɗ Joatam. Joatam vuɗ Akaz. Akaz vuɗ Ezekias.",
+      text: "Oziyas vuɗ Joatam. Joatam vuɗ Akaz. Akaz vuɗ Ezekias. Ezekias vuɗ Manasse.",
       french:
         "Ozias engendra Joatham; Joatham engendra Achaz; Achaz engendra Ézéchias;",
     },
     {
-      id: "1-1-10",
+      id: "40-1-10",
       verse: 10,
-      text: "Ezekias vuɗ Manasse. Manasse vuɗ Amon. Amon vuɗ Josias.",
+      text: "Manasse vuɗ Amon. Amon vuɗ Josias.",
       french:
         "Ézéchias engendra Manassé; Manassé engendra Amon; Amon engendra Josias;",
     },
     {
-      id: "1-1-11",
+      id: "40-1-11",
       verse: 11,
       text: "Josias vuɗ Yekoniya u gorsemma vuɗ ni, suuna yowosi ha ki Babilon.",
       french:
         "Josias engendra Jéchonias et ses frères, au temps de la déportation à Babylone.",
     },
     {
-      id: "1-1-12",
+      id: "40-1-12",
       verse: 12,
       text: "Asi yowosi ha Babilon yow ni, Yekoniya vuɗ Salatiyel. Salatiyel vuɗ Zorobabel.",
       french:
         "Après la déportation à Babylone, Jéchonias engendra Salathiel; Salathiel engendra Zorobabel;",
     },
     {
-      id: "1-1-13",
+      id: "40-1-13",
       verse: 13,
       text: "Zorobabel vuɗ Abiud. Abiud vuɗ Eliakim. Eliakim vuɗ Azor.",
       french:
         "Zorobabel engendra Abiud; Abiud engendra Éliakim; Éliakim engendra Azor;",
     },
     {
-      id: "1-1-14",
+      id: "40-1-14",
       verse: 14,
       text: "Azor vuɗ Sadok. Sadok vuɗ Asim. Asim vuɗ Eliud.",
       french:
         "Azor engendra Sadok; Sadok engendra Achim; Achim engendra Éliud;",
     },
     {
-      id: "1-1-15",
+      id: "40-1-15",
       verse: 15,
       text: "Eliud vuɗ Eleazar. Eleazar vuɗ Matan. Matan vuɗ Yakob.",
       french:
         "Éliud engendra Éléazar; Éléazar engendra Matthan; Matthan engendra Jacob;",
     },
     {
-      id: "1-1-16",
+      id: "40-1-16",
       verse: 16,
       text: "Yakob vuɗ Yosef ma njuf Marina. Mari vuɗ Yesu ma Lona Fuurumma.",
       french:
         "Jacob engendra Joseph, l'époux de Marie, de laquelle est né Jésus, qui est appelé Christ.",
     },
     {
-      id: "1-1-17",
+      id: "40-1-17",
       verse: 17,
       text: "Col ay eg Abraham mbay tew kay David na ni, somoyosi doogo yam fiɗi. Eg David mba hu yow mayra asi yowosi Babilonda ni, somoyosi doogo yam fiɗi. Duk ɦoŋ yowra Babilonda mba tew vuɗta vi Yesu ni, somoyosi doogo yam fiɗi.",
       french:
         "Il y a donc en tout quatorze générations depuis Abraham jusqu'à David, quatorze générations depuis David jusqu'à la déportation à Babylone, et quatorze générations depuis la déportation à Babylone jusqu'au Christ.",
     },
     {
-      id: "1-1-18",
+      id: "40-1-18",
       verse: 18,
       text: "Ni vuɗta vi Yesura. Yosef gi ɗum Mari, bay vaɗ tuwa. Ndaɗ vi wiina u Musuk Lonara.",
       french:
         "Voici de quelle manière arriva la naissance de Jésus Christ. Marie, sa mère, ayant été fiancée à Joseph, se trouva enceinte, par la vertu du Saint Esprit, avant qu'ils eussent habité ensemble.",
     },
     {
-      id: "1-1-19",
+      id: "40-1-19",
       verse: 19,
       text: "Njufuɗ Yosef ni sa ma ɗilina, nam min a sa hum boy namma ki koloɗi. Nam hal a busuɗ ki bay gi u boy namma kiyo.",
       french:
         "Joseph, son époux, qui était un homme de bien et qui ne voulait pas la diffamer, se proposa de rompre secrètement avec elle.",
     },
     {
-      id: "1-1-20",
+      id: "40-1-20",
       verse: 20,
       text: "Nam ka saa kay boy namma may ni, sa sunda huulona mba dam doyra ana, «Yosef, goŋ Davidna, li ndaa a vi caaŋga Mari varaŋgaɗi, vaɗ wiina huruɗ ni Musuk Lonara.",
       french:
         "Comme il y pensait, voici, un ange du Seigneur lui apparut en songe, et dit: Joseph, fils de David, ne crains pas de prendre avec toi Marie, ta femme, car l'enfant qu'elle a conçu vient du Saint Esprit;",
     },
     {
-      id: "1-1-21",
+      id: "40-1-21",
       verse: 21,
       text: "Ndaɗ hin vuɗ goona, aŋ tinim sem ana Yesu, kayme nam hin suɗ suu mamsina ki hu coosira.»",
       french:
         "elle enfantera un fils, et tu lui donneras le nom de Jésus; c'est lui qui sauvera son peuple de ses péchés.",
     },
     {
-      id: "1-1-22",
+      id: "40-1-22",
       verse: 22,
       text: "Dla ndaɗta li wa na ni, faɗ ki ni vun boy ma sa jok vun Lona ma di ana,",
       french:
         "Tout cela arriva afin que s'accomplît ce que le Seigneur avait annoncé par le prophète:",
     },
     {
-      id: "1-1-23",
+      id: "40-1-23",
       verse: 23,
       text: "«Goowayra hin vi wiina, ndaɗ hin vuɗ goona, tinim sem ana Emmanuel,» (ni ana Lona ka u aygiya).",
       french:
         "Voici, la vierge sera enceinte, elle enfantera un fils, et on lui donnera le nom d'Emmanuel, ce qui signifie Dieu avec nous.",
     },
     {
-      id: "1-1-24",
+      id: "40-1-24",
       verse: 24,
       text: "Yosef dliɗ kolo ni, li ko sa sunda huulona damba na. Nam vi caamba, Mari, varamu.",
       french:
         "Joseph s'étant réveillé fit ce que l'ange du Seigneur lui avait ordonné, et il prit sa femme avec lui.",
     },
     {
-      id: "1-1-25",
+      id: "40-1-25",
       verse: 25,
       text: "Nam buu ka u ndaɗɗi, gak gak ndaɗ vuɗ goroɗna kiyo. Yosef tinim sem ana Yesu.",
       french:
