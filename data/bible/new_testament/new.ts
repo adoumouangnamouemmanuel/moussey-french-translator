54f44f73b85b0bf49a0aa839f5e0b4101e53435e
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2025-04-25 05:06:33 +0000

    new testament

---
+++ data/bible/new_testament/new.ts
@@ -0,0 +1,27 @@
+import { matthew } from "./matthew/matthew";
+
+export const new_testament = {
+    "matthew": matthew,
+    // "mark": mark,
+    // "luke": luke,
+    // "john": john,
+    // "acts": acts,
+    // "romans": romans,
+    // "corinthians": corinthians,
+    // "galatians": galatians,
+    // "ephesians": ephesians,
+    // "philippians": philippians,
+    // "colossians": colossians,
+    // "thessalonians": thessalonians,
+    // "timothy": timothy,
+    // "titus": titus,
+    // "philemon": philemon,
+    // "hebrews": hebrews,
+    // "james": james,
+    // "peter": peter,
+    // "john": john,
+    // "jude": jude,
+    // "revelation": revelation,
+    // "apocalypse": apocalypse,
+    // "apocrypha": apocrypha,
+};
