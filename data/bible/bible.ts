db7e02a5912b92461950106c5753736324442b83
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2025-04-25 05:07:19 +0000

    Bible

--- data/bible/bible.ts
+++ data/bible/bible.ts
@@ -1,6 +1,7 @@
 import { old_testament } from "./old_testament/old";
+import { new_testament } from "./new_testament/new";
 
 export const bible = {
   "old": old_testament,
-  // new_testament
+  "new": new_testament,
 };
